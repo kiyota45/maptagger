cef736a139feaa007db52941ca3deedb4f856230
kiyota45 <studio.krescent@gmail.com>
2015-08-08 22:39:22 +0900

    add balloon

--- detail.php
+++ detail.php
@@ -1,20 +1,22 @@
 <?php
 require_once('lib.php');
 header_print('家ログ 詳細', array());
-
+$id = $_GET['id'];
 echo('<a href="contact.php?id=XXXX">contact</a>');
+echo("$id");
 
 $ncomments = 10; /* XXX */
+	echo("<div class=\"balloon-wrapper\">\n");
 for ($i = 0; $i < $ncomments; $i++) {
-	echo("<div class=\"container\" >\n");
-	echo("<div class=\"commentator\">\n");
-	echo("<img alt=\"fugafuga\" src=\"hogehoge\" />\n");
-	echo("</div>\n");
-	echo("<div class=\"comment\">");
-	echo('hogehoge');
-	echo("</div>\n");
-	echo("</div>\n");
+	if($i % 2 == 0)
+		echo("<p class=\"balloon-left\">");
+	else
+		echo("<p class=\"balloon-right\">");
+
+	echo("また、資源をエネルギーに変換するためには、要らないものを空気中に排出しなくてはなりません。二酸化炭素などの排出物は、大気汚染を引き起こし、やがては地球全体の気温を徐々にあげてしまいます。これが地球温暖化と呼ばれるもので、異常気象や農作物への影響、海水面の上昇、紫外線の問題など、さまざまな問題の原因となっています。人間だけでなく、すべての生き物の住みやすい環境を守るためにも、エネルギーの節約は必要なのです。 ");
+	echo("</p>\n");
 }
+	echo("</div>\n");
 
 footer_print();
 ?>
